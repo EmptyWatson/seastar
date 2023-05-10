a0f23aa899c700329806c2823aa503c4bc691e1c
zky_watson <1424599184@qq.com>
2023-05-10 10:12:33 +0800

    [add] 增加构建脚本，方便使用；

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mode=$1
+source /opt/rh/devtoolset-11/enable
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export BOOST_ROOT=/opt/boost
+
+if [[ "${mode}" == "release" ]]; then
+    echo "use release mode to build"
+    ./configure.py --mode=release
+    ninja -C build/release -j4
+    ninja install -C build/release
+else
+    echo "use debug mode to build"
+    ./configure.py --mode=debug
+    ninja -C build/debug -j4
+    ninja install -C build/debug
+fi
