f2f4ad5fe0577ceca2fcf2d9584baaf71509ae10
zky_watson <1424599184@qq.com>
2023-06-06 12:10:04 +0800

    [opt] 优化编译参数，减少资源开销；

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ export BOOST_ROOT=/opt/boost
 
 if [[ "${mode}" == "release" ]]; then
     echo "use release mode to build"
-    ./configure.py --mode=release
+    #./configure.py --mode=release
+    ./configure.py --disable-hwloc --cflags="-DSEASTAR_DEFAULT_ALLOCATOR -fvisibility=default" --mode=release
     ninja -C build/release -j4
     ninja install -C build/release
 else
     echo "use debug mode to build"
-    ./configure.py --mode=debug
+    #./configure.py --mode=debug 
+    ./configure.py --disable-hwloc --cflags="-DSEASTAR_DEFAULT_ALLOCATOR -fvisibility=default" --mode=debug 
     ninja -C build/debug -j4
     ninja install -C build/debug
 fi
